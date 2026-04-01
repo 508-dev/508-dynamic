62edfe360266e4dc15fbfd3562cfa9c5103b19c3
Michael Wu <michaelmwu@gmail.com>
2026-04-01 15:45:02 +0900

    Remove unsupported Astro Tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://508.dev",
   trailingSlash: "never",
+  vite: {
+    plugins: [tailwindcss()],
+  },
   build: {
     format: "file",
   },
-  integrations: [tailwind()],
 });
